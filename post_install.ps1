db9ea06ef49effee7232b9612034524e634aa059
Teo <teo.orthlieb@gmail.com>
2025-04-04 11:14:00 +0200

    test3

--- post_install.ps1
+++ post_install.ps1
@@ -10,4 +10,14 @@ $regPathAdmin = "HKCR\*\Shell\Batch Rename\command"
 $regPathUser = "HKCU\Software\Classes\*\Shell\Batch Rename\command"
 $regValue = "`"$guiPath`" `"%1`""
 
-Write-Host $regValue
+# Function to check if the script is running as admin
+function Test-IsAdmin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal [Security.Principal.WindowsIdentity]::GetCurrent()
+    return $currentUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
+if (Test-IsAdmin) {
+    Write-Host "Running as admin, setting registry key in HKCR."
+} else {
+    Write-Host "Not running as admin, setting registry key in HKCU."
+}
