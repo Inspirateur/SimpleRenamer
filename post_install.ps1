a3bc7ce054019b6769d00101da973c5ffcc68145
Teo <teo.orthlieb@gmail.com>
2025-04-04 11:05:07 +0200

    test 2

--- post_install.ps1
+++ post_install.ps1
@@ -1,2 +1,13 @@
-# Check if we can get post install variables like this
-Write-Host $dir
+# Scoop post-install script for setting registry key
+# This script sets the context menu option for "Batch Rename" to point to the installed gui.exe
+
+# Get the installation path of the current script
+$installDir = Split-Path -Parent $dir
+$guiPath = Join-Path -Path $installDir -ChildPath "gui.exe"
+
+# Define the registry paths and values
+$regPathAdmin = "HKCR\*\Shell\Batch Rename\command"
+$regPathUser = "HKCU\Software\Classes\*\Shell\Batch Rename\command"
+$regValue = "`"$guiPath`" `"%1`""
+
+Write-Host $regValue
